48b1503be13689fbd5d2c4b6408b7179343ae831
JSifalda <sifalda.jiri@gmail.com>
2013-02-24 21:14:19 +0100

    Add configs

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -22,10 +22,9 @@ $configurator->createRobotLoader()
 	->register();
 $configurator->addParameters(array('appDir' => __DIR__));
 
-$configurator->addConfig(__DIR__ . '/AppModule/config/config.neon');
-$configurator->addConfig(__DIR__ . '/AppModule/config/services.neon');
-$configurator->addConfig(__DIR__ . '/AppModule/config/factories.neon');
-if(file_exists($configDev = __DIR__ . '/AppModule/config/config.dev.neon'))
+$configurator->addConfig(__DIR__ . '/config/bundles.neon');
+$configurator->addConfig(__DIR__ . '/config/config.neon');
+if(file_exists($configDev = __DIR__ . '/config/config.dev.neon'))
 	$configurator->addConfig($configDev);
 
 $container = $configurator->createContainer();
