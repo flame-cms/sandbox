4865dcae1e2a3f343a0d55d84e161d0ba3849d35
JSifalda <sifalda.jiri@gmail.com>
2013-02-24 17:04:16 +0100

    Add Basic presenters & templates into AppModule

---
+++ app/AppModule/AdminModule/presenters/DashboardPresenter.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Flame\CMS\AdminModule;
+
+/**
+* Dashboard for administration
+*/
+class DashboardPresenter extends AdminPresenter
+{
+
+	public function actionDefault()
+	{
+	}
+}
