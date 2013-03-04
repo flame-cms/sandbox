160ff310e86795b7764350a3afc7d2e2ef25e86f
JSifalda <sifalda.jiri@gmail.com>
2013-03-04 19:06:14 +0100

    Add post bundle, web loader, doctrine extension

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -12,7 +12,13 @@ require __DIR__ . '/../libs/autoload.php';
 
 $configurator = new \Flame\Config\Configurator();
 // $configurator->setDebugMode(TRUE);
-$configurator->registerExtension('doctrine', '\Flame\Doctrine\Config\Extension');
+
+$doctrineExtension = new \Flame\Doctrine\Config\Extension;
+$doctrineExtension->install($configurator);
+
+$webloaderExtension = new \WebLoader\Nette\Extension();
+$webloaderExtension->install($configurator);
+
 $configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 $configurator->createRobotLoader()->addDirectory(__DIR__)->register();
@@ -22,6 +28,7 @@ $configurator->addConfig(__DIR__ . '/AppBundle/config/config.neon');
 $configurator->addConfig(__DIR__ . '/LinkBundle/config/config.neon');
 $configurator->addConfig(__DIR__ . '/SettingBundle/config/config.neon');
 $configurator->addConfig(__DIR__ . '/UserBundle/config/config.neon');
+$configurator->addConfig(__DIR__ . '/PostBundle/config/config.neon');
 if(file_exists($configDev = __DIR__ . '/AppBundle/config/config.dev.neon'))
 	$configurator->addConfig($configDev);
 
